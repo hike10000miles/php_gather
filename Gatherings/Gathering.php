b9f8fb21149a4ceb2191706a52e6b68f2beb8dee
Kevin Sanabria <Kevin Sanabria>
2017-04-21 16:33:10 -0400

    fixed path to gathering

--- Gatherings/Gathering.php
+++ Gatherings/Gathering.php
@@ -14,22 +14,26 @@ if (!isset($_SESSION['user_id']) && !isset($_SESSION['gatherid'])) {
     header('Location: create.php');
 }
 
-
-
 $db = Connect::dbConnect();
 
+$_SESSION['LoggedIn']['UserId'];
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+}
+
 $eventController = new EventConnect($db);
-$thisuserDetails = new gatheringsController($db);
+$gathercontroller = new gatheringsController($db);
 
-$usersDetails = $thisuserDetails->selectUserDetails($db, $_SESSION['user_id']);
+$usersDetails = $gathercontroller->selectUserDetails($db, $_SESSION['LoggedIn']['UserId']);
 
-$row = $gatheringDetails->selectGathering($db, $_SESSION['gatherid']);
+$row = $gathercontroller->selectGathering($db,$id);
 
-$fetchUsers = $gatheringDetails->get_GatheringusersModified($db, $_SESSION['gatherid']);
+$fetchUsers = $gathercontroller->get_GatheringusersModified($db, $id);
 
-$fetchEvents = $eventsInGathering->get_GatheringusersModified($db, $_SESSION['gatherid']);
+$fetchEvents = $gathercontroller->get_GatheringusersModified($db, $id);
 
-$events = $getEventsforGather->getgatheringsEvents($db);
+$events = $gathercontroller->getgatheringsEvents($db);
 
 ?>
 
