28cae09eb05baaccdc5584d7471dc4055b61ef54
[pritpalk] <[pritpalkaur03@gmail.com]>
2017-04-21 16:49:06 -0400

    Bug fix

--- Business/Business.php
+++ Business/Business.php
@@ -31,13 +31,13 @@ if(isset($_SESSION['LoggedIn']['BusinessId'])) {
     }
 } elseif (isset($_SESSION['LoggedIn']['UserId']) && isset($_GET['id'])) {
     try {
-        $businessdetails = $businessview->getBusinessInfo($db,$_GET['id']);
+        $businessdetails = $businessview->getBusinessInfo($db, $_GET['id']);
         $events = $eventController->getEventList($_GET['id']);
         $reviews = $reviewController->displayreviewsbybusinessid($_GET['id']);
     } catch (Exception $e) {
         $error = $e;
     }
-
+}
 if(isset($_POST['like'])) {
     $row4 = $reviewController->getlikes($_POST['post_id']);
 }
@@ -60,9 +60,9 @@ $totalreview = $reviewController->getCountReviews($businessId);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <?php include(__root."views/components/globalhead.php"); ?>
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
@@ -70,7 +70,7 @@ $totalreview = $reviewController->getCountReviews($businessId);
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Business | Gather</title>
 </head>
-<>
+<body>
 <?php include(__root."views/components/userheader.php"); ?>
 <div class="container">
     <?php if(is_a($error, "Exception")):?>
@@ -399,10 +399,6 @@ $totalreview = $reviewController->getCountReviews($businessId);
     <?php endforeach; ?>
     <?php include(__root."views/components/footer.php"); ?>
 </div>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src='<?php echo __httpRoot . "assest/"; ?>bootstrap/js/bootstrap.min.js'></script>
 
 </div>
 </body>
